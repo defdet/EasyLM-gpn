265951e966ec268c23653e2b650cdc23b4f52322
Max <bolgov1458@yandex.ru>
2025-02-19 11:20:47 +0300

    Update llama_train.py

--- EasyLM/models/llama/llama_train.py
+++ EasyLM/models/llama/llama_train.py
@@ -65,8 +65,9 @@ def main(argv):
     print('Loaded tokenizer')
     dataset = DatasetFactory.load_dataset(FLAGS.train_dataset, tokenizer)
     print(f'Shoud attempt to load the dataset state with {FLAGS.load_dataset_state}')
-    print('Loading dataset state')
-    dataset.load_state_dict(mlxu.load_pickle('/kaggle/input/more-ds-states/dataset_20000.pkl'))
+    if False:
+        print('Loading dataset state')
+        dataset.load_state_dict(mlxu.load_pickle('/kaggle/input/more-ds-states/dataset_20000.pkl'))
 
     if FLAGS.eval_steps > 0:
         eval_dataset = DatasetFactory.load_dataset(
